8e2ea4469e8a21936c181ae61f1c25786472b066
teopousa <teopousa@live.com>
2023-06-19 21:55:31 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+console.warn('Esto es una advertencia.')
+console.error('Houston, tenemos un problema.')
+
+let trabajoPractico = "de Js"
+const enDonde = "Coderhouse"
+
+
+let username = prompt( "Cual es tu nombre de usuario🤔:" )
+if (username == "Teo Pousa") {  
+    console.log( "Bienvenido jefe 😁")
+}else{
+    console.log( "No sos Teo Pousa.❌")
+}
+
+const clubes = ["Boca Juniors", "River Plate", "Independiente", "Racing", "San Lorenzo", "Velez", "Estudiantes", "Talleres", "Belgrano", "Lanus", "Rosario Central"]
+for (let i=0; i<=20; i++){
+    console.log (clubes [i])
+}
